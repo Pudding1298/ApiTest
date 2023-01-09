65855ba47a9e6fec50e4e89f4a28e9311b2d2e06
Francisco Bazán <azgardb@gmail.com>
2023-01-08 20:04:54 -0600

    Agregar archivos de proyecto.

---
+++ ApiTest/Controllers/VistaController.cs
@@ -0,0 +1,20 @@
+﻿using DA.Models;
+using DA.Providers;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VistaController
+    {
+        [HttpGet("GetVista")]
+
+        public async Task<string> GetVista() { 
+            TestProvider vista = new TestProvider();
+            IEnumerable<vTimeSeries> resultado = await vista.LeerVista();
+            return JsonConvert.SerializeObject(resultado);
+        }
+    }
+}
---
+++ DA/Providers/TestProvider.cs
@@ -0,0 +1,29 @@
+﻿using DA.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA.Providers
+{
+    public class TestProvider
+    {
+        public async Task<IEnumerable<vTimeSeries>> LeerVista() 
+        {
+            try 
+            {
+                using (AdventureWorksEntities context = new AdventureWorksEntities())
+                {
+                    return await context.vTimeSeries.ToListAsync();
+                }
+            }
+            catch(Exception ex) 
+            {
+                throw ex;
+            }
+        }
+    }
+}
